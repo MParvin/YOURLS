3c6ea7bd1f2894c33f743449b0a278590007c892
Mohammad Parvin <m.parvin986@gmail.com>
2020-04-14 18:42:52 +0430

    add bootstrap

--- includes/functions-html.php
+++ includes/functions-html.php
@@ -125,6 +125,7 @@ function yourls_html_head( $context = 'index', $title = '' ) {
 		var ajaxurl  = '<?php echo yourls_admin_url( 'admin-ajax.php' ); ?>';
 	//]]>
 	</script>
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
 	<?php yourls_do_action( 'html_head', $context ); ?>
 </head>
 <body class="<?php echo $context; ?> <?php echo $bodyclass; ?>">
